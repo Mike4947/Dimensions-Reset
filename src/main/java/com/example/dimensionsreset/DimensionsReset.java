cf62df51e6d7112484fefe88e4c7d9caeb10b26a
Mike <mik481@hotmail.com>
2025-06-11 00:08:50 -0400

    Update DimensionsReset.java
    idk

--- src/main/java/com/example/dimensionsreset/DimensionsReset.java
+++ src/main/java/com/example/dimensionsreset/DimensionsReset.java
@@ -12,41 +12,39 @@ import java.util.List;
 
 public final class DimensionsReset extends JavaPlugin {
 
-    // This field will hold the instance of our command handler
+    // These fields will hold the instances of our other classes
     private DRCommand drCommand;
+    private DataManager dataManager;
 
     @Override
     public void onEnable() {
-        // Save the default config.yml to the plugin's folder
+        // First, save the default config file
         saveDefaultConfig();
 
-        // Create the class that will handle all the command's logic
-        this.drCommand = new DRCommand(this);
+        // Second, initialize our new Data Manager
+        this.dataManager = new DataManager(this);
 
-        // --- Programmatic Command Registration ---
+        // Third, create the command handler, passing BOTH the plugin and dataManager
+        this.drCommand = new DRCommand(this, this.dataManager);
+
+        // Command Registration (this part is unchanged)
         try {
-            // Get the server's internal "command map"
             final Field bukkitCommandMap = getServer().getClass().getDeclaredField("commandMap");
             bukkitCommandMap.setAccessible(true);
             CommandMap commandMap = (CommandMap) bukkitCommandMap.get(getServer());
 
-            // Create our command object using reflection
             Constructor<PluginCommand> constructor = PluginCommand.class.getDeclaredConstructor(String.class, Plugin.class);
             constructor.setAccessible(true);
             PluginCommand command = constructor.newInstance("dr", this);
 
-            // Set the command's properties
             command.setAliases(List.of("dims", "dreset"));
             command.setDescription("Main command for DimensionsReset.");
             command.setUsage("/dr <subcommand>");
             command.setPermission("dimensionsreset.admin");
-
-            // Register our new command in the server's command map
+            
             commandMap.register(this.getDescription().getName(), command);
-
-            // Set our DRCommand class as the one that executes the command's logic
+            
             command.setExecutor(this.drCommand);
-            // Set our DRCommand class to also handle tab completion
             command.setTabCompleter(this.drCommand);
 
         } catch (Exception e) {
@@ -54,12 +52,14 @@ public final class DimensionsReset extends JavaPlugin {
             e.printStackTrace();
             return;
         }
-
-        // --- Register the new event listener ---
-        // This handles players logging out while in preview mode.
+        
+        // Register the event listener to handle players quitting in preview mode
         getServer().getPluginManager().registerEvents(new PlayerListener(this.drCommand), this);
 
-
+        // Start our new Scheduler Task and pass it all the required components
+        // It will run every minute (1200 ticks = 60 seconds * 20 ticks/sec)
+        new SchedulerTask(this, dataManager, drCommand).runTaskTimer(this, 0L, 1200L);
+        
         getLogger().info("DimensionsReset has been enabled successfully!");
     }
 
